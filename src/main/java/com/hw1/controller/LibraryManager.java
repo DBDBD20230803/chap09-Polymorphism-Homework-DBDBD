4a35264a76b10d11a7110b1941cdb21071ae3864
ksostuft <ksostuft@gmail.com>
2023-08-17 22:31:38 +0900

    rentBook 기능 테스트 완료

--- src/main/java/com/hw1/controller/LibraryManager.java
+++ src/main/java/com/hw1/controller/LibraryManager.java
@@ -1,11 +1,22 @@
 package com.hw1.controller;
 
+import com.hw1.model.dto.AniBook;
 import com.hw1.model.dto.Book;
+import com.hw1.model.dto.CookBook;
 import com.hw1.model.dto.Member;
 
 public class LibraryManager {
     private Member mem;
+
     private Book[] bList = new Book[5];
+
+    {
+        bList[0] = new CookBook("백종원의 집밥", "백종원", "tvN", true);
+        bList[1] = new AniBook("한번 더 해요", "미티", "원모어", 19);
+        bList[2] = new AniBook("루피의 원피스", "루피", "japan", 12);
+        bList[3] = new CookBook("이혜정의 얼마나 맛있게요", "이혜정", "문학", false);
+        bList[4] = new CookBook("최현석 날 따라해봐", "최현석", "소금책", true);
+    }
 
     public void insertMember(Member mem) {
 
@@ -24,6 +35,20 @@ public class LibraryManager {
     }
 
     public int rentBook(int index) {
-        return 0;
+        int result = 0;
+        if(bList[index] instanceof AniBook) {
+            AniBook aniBook = (AniBook) bList[index];
+
+            if(aniBook.getAccessAge() > mem.getAge()) {
+                result = 1;
+            }
+        } else if(bList[index] instanceof CookBook) {
+            CookBook cookBook = (CookBook) bList[index];
+            if(cookBook.isCoupon() == true) {
+                mem.setCouponCount(mem.getCouponCount() + 1);
+                result = 2;
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/hw1/view/LibraryMenu.java
+++ src/main/java/com/hw1/view/LibraryMenu.java
@@ -22,6 +22,18 @@ public class LibraryMenu {
     }
 
     public void rentBook() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("대여할 도서 번호를 입력해주세요 : ");
+        int index = sc.nextInt();
 
+        int result = lm.rentBook(index);
+
+        if(result == 0) {
+            System.out.println("성공적으로 대여되었습니다.");
+        } else if(result == 1) {
+            System.out.println("나이 제한으로 대여 불가능입니다.");
+        } else if(result == 2) {
+            System.out.println("성공적으로 대여되었습니다. 요리학원 쿠폰이 발급되었습니다. 마이페이지를 통해 확인하세요.");
+        }
     }
 }
